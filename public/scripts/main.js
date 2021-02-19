09a0cc4b29f8c3b1a8de230c27f587faa09e651d
Keiran Hollenbeck <kmankeiran@gmail.com>
2021-02-19 18:39:36 -0500

    finishing up functions, rewriting a couple things and adding some css

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,6 +3,9 @@ import ChatMessage from "./components/TheMessageComponent.js"
 (() => {
     console.log('fired');
 
+    // Get the username that was inputted and save it in a variable
+    const name = localStorage.getItem('name');
+
     // load the socket library and make a connection
     const socket = io();
 
@@ -18,16 +21,17 @@ import ChatMessage from "./components/TheMessageComponent.js"
         vm.messages.push(message);
     }
 
-    function setIsSomeoneTyping(message) {
-        console.log("someone else is typing");
-        vm.isSomeoneElseTyping = message.istyping && message.name != localStorage.getItem('name');
+    function setIsSomeoneTyping(msg) {
+       // Check if the message is coming from someone else by making sure the socket ID doesn't match ours
+        if(vm.socketID != msg.id) {
+            //Set the isSomeoneElseTyping to what the message has
+            vm.isSomeoneElseTyping = msg.message.istyping;
+        }
     }
 
     const vm = new Vue({
         data: {
             messages: [],
-            nickname: "",
-            username: "",
             socketID: "",
             message: "",
             isTyping: false,
@@ -41,8 +45,8 @@ import ChatMessage from "./components/TheMessageComponent.js"
         methods: {
             dispatchMessage() {
                 //debugger;
-                socket.emit('chatmessage', { content: this.message, name: localStorage.getItem('name') || "Anonymous" });
-                socket.emit('istyping', { istyping: false, name: localStorage.getItem('name') || "Anonymous" });
+                socket.emit('chatmessage', { content: this.message, name: name || "Anonymous" });
+                socket.emit('istyping', { istyping: false, name: name || "Anonymous" });
                 this.isTyping = false;
 
                 this.message = "";
@@ -51,12 +55,12 @@ import ChatMessage from "./components/TheMessageComponent.js"
                 // If the sentence is greater than 0 and the user is inputing something, let Vue know they are typing
                 if(this.message.length > 0 && !this.isTyping) {
                     this.isTyping = true;
-                    socket.emit('istyping', { istyping: true, name: localStorage.getItem('name') || "Anonymous" });
+                    socket.emit('istyping', { istyping: true, name: name || "Anonymous" });
                 } 
                 // If Vue thinks they are typing but the message is the length of 0, then tell Vue they are no longer typing. Like backspacing.
                 else if(this.isTyping && this.message.length==0){
                     this.isTyping = false;
-                    socket.emit('istyping', { istyping: false, name: localStorage.getItem('name') || "Anonymous" });
+                    socket.emit('istyping', { istyping: false, name: name || "Anonymous" });
                 }
             }
         },
